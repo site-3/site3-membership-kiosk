b42a4712561836dacb3b32657e42e18ff026aba7
katee <hello@kate.io>
2015-11-14 18:48:29 -0500

    Hides RFID text box, can be shown with menu item.

--- app/src/main/java/ca/site3/site3membershipkiosk/SignupActivity.java
+++ app/src/main/java/ca/site3/site3membershipkiosk/SignupActivity.java
@@ -11,6 +11,9 @@ import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -175,6 +178,25 @@ public class SignupActivity extends AppCompatActivity implements OnClickListener
         switch(v.getId()) {
             case R.id.submit:
                 submit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_signup_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.edit_rfid:
+                rfid.setVisibility(View.VISIBLE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
