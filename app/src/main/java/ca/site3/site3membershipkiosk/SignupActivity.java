aea8e2343e631def1194212aa595271c32ac068f
katee <hello@kate.io>
2015-11-14 15:08:22 -0500

    Reads id from scanned tag.

--- app/src/main/java/ca/site3/site3membershipkiosk/SignupActivity.java
+++ app/src/main/java/ca/site3/site3membershipkiosk/SignupActivity.java
@@ -1,13 +1,62 @@
 package ca.site3.site3membershipkiosk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = SignupActivity.class.getSimpleName();
+
+    PendingIntent pendingIntent;
+    IntentFilter[] intentFiltersArray;
+    String[][] techListsArray;
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        intentFiltersArray = new IntentFilter[] { new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED) };
+        techListsArray = new String[][] { new String[] { NfcF.class.getName() } };
+    }
+
+    public void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    public void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+    }
+
+    public void onNewIntent(Intent intent) {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Toast.makeText(SignupActivity.this, "Tag scanned", Toast.LENGTH_SHORT).show();
+
+        String hexTagId = tagIdToHex(tagFromIntent.getId());
+
+        Log.e(LOG_TAG, hexTagId);
+    }
+
+    private String tagIdToHex(byte[] tagId) {
+        String hexTagId = "";
+        for (byte b : tagId) {
+            hexTagId = hexTagId + String.format("%02x", b);
+        }
+        return hexTagId;
     }
 }
