ba041f30f8063e30d6cad03f525bed32116c1825
katee <hello@kate.io>
2015-11-14 19:59:31 -0500

    Adds fullscreen success activity.

---
+++ app/src/main/java/ca/site3/site3membershipkiosk/SuccessActivity.java
@@ -0,0 +1,14 @@
+package ca.site3.site3membershipkiosk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SuccessActivity extends AppCompatActivity {
+    private static final String LOG_TAG = SuccessActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
